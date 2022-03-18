c636df19f5a38795ac09d8cb292ffd2a32195c30
kelvinbiffi <kelvin.biffi@firstleaf.com>
2022-03-17 23:20:02 -0300

    added about page info

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -3,8 +3,23 @@ import Container, { ETextAlign } from "../Components/Container";
 
 const About = () => {
   return (
-    <Container textAlign={ETextAlign.CENTER}>
-      <h1>About</h1>
+    <Container textAlign={ETextAlign.LEFT}>
+      <>
+        <h1>Sobre</h1>
+        <hr/>
+        <p>
+          O Mental Kare é um Hub de saúde mental com 3 principais objetivos:
+          <br/>
+          <br/>
+          1 - Agregar conteúdo de qualidade e verificado sobre TEA, TDAH, TOD, entre outros, como artigos científicos, vídeos, livros, entre outros que ajudem as pessoas a entenderem o que sao esses assuntos.
+          <br/>
+          <br/>
+          2 - Auxiliar famílias que precisam de suporte sobre o assunto
+          <br/>
+          <br/>
+          3 - Criar um sistema nacional que interligue de pessoas que precisam de tratamento ou estão em busca com profissionais qualificados que podem auxiliar nesse momento
+        </p>
+      </>
     </Container>
   )
 }
