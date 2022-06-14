7bc35b52670d1a70149b93963ebd5c709fb9f615
kelvinbiffi <kelvinbiffi.developer@gmail.com>
2022-06-14 00:25:53 -0300

    added account component

--- src/Pages/Accounts.tsx
+++ src/Pages/Accounts.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { ReactElement } from "react";
+import AccountCard, { IAccountCard } from "../Components/AccountCard";
+import accounts from "../Data/Accounts";
 
 import { Col, Container, Row } from "react-bootstrap";
 
@@ -6,9 +8,21 @@ const Accounts = () => {
   return (
     <Container>
       <Row>
-        <Col>
-          Em construção...
-        </Col>
+        {accounts.map((props: IAccountCard): ReactElement => {
+          const { id, image, title, author, buyLink, descr } = props;
+          return (
+            <Col key={id} xs={12} lg={6}>
+              <AccountCard
+                id={id}
+                image={image}
+                title={title}
+                author={author}
+                buyLink={buyLink}
+                descr={descr}
+              />
+            </Col>
+          );
+        })}
       </Row>
     </Container>
   )
